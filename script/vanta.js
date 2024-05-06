119a07b9d66f23f9ebc7b5816cfbc27335f9d481
3punk <112157084+3punk@users.noreply.github.com>
2024-05-06 22:55:53 +0700

    Add files via upload

---
+++ script/vanta.js
@@ -0,0 +1,13 @@
+VANTA.FOG({
+  el: "#pack",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0xff00f1,
+  midtoneColor: 0xff0087,
+  baseColor: 0xd6b8d9,
+  blurFactor: 0.62,
+  speed: 1.50
+})
